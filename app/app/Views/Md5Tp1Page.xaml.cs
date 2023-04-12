47ebb0aa47f553123d9ec3955128ee45b562a85b
Benoît Rataux <benoit.rataux@gmail.com>
2023-04-12 15:21:16 +0200

    md5tp1: formulaire et images

---
+++ app/app/Views/Md5Tp1Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Md5Tp1Page : ContentPage
+	{
+		public Md5Tp1Page ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
